f41977738d274f7e108e077afdf2a46d6df4c8f2
gencatcloud <suport.cloud@gencat.cat>
2017-05-22 11:59:48 +0200

    Initial commit

---
+++ docker-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# setup directory for data
+mkdir -p /data
+chown -R nginx:0 /data
+chmod g+w -R /data
+chown -R nginx:0 /usr
+chown -R nginx:0 /var
+
+chgrp -R 0 /usr
+chmod -R g+rw /usr
+find /usr -type d -exec chmod g+x {} +
+
+chgrp -R 0 /var
+chmod -R g+rw /var
+find /var -type d -exec chmod g+x {} +
+
